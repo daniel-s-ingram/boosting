c8032f7cc83f40b7fbeb7270a4135056940c952b
Daniel Ingram <ingramds@appstate.edu>
2019-04-11 13:53:55 -0400

    Add example of boost::thread::disable_interruption

--- parallel_programming/boost_thread_interrupt.cpp
+++ parallel_programming/boost_thread_interrupt.cpp
@@ -23,12 +23,34 @@ void thread()
     }
 }
 
+void other_thread()
+{
+    boost::this_thread::disable_interruption no_interruption;
+    try
+    {
+        for(int i = 0; i < 5; ++i)
+        {
+            std::cout << i << '\n';
+            wait(1);
+        }
+    }
+    catch(boost::thread_interrupted&)
+    {
+        std::cout << "Interrupted!\n";
+    }
+}
+
 int main(int argc, char** argv)
 {
     boost::thread t{thread};
     wait(3);
     t.interrupt();
     t.join();
+
+    boost::thread ot{other_thread};
+    wait(3);
+    ot.interrupt();
+    ot.join();
 
     return 0;
 }
