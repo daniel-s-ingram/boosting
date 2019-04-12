38e874b7025c4ca4e71c43dc25d4afc11d5a1300
Daniel Ingram <ingramds@appstate.edu>
2019-04-12 15:57:32 -0400

    Add comparison to single-thread sum

--- parallel_programming/boost_sum.cpp
+++ parallel_programming/boost_sum.cpp
@@ -33,24 +33,34 @@ std::uint64_t operator+(std::uint64_t lhs, const Adder& rhs)
 
 int main(int argc, char** argv)
 {
-    boost::timer::cpu_timer timer;
+    std::uint64_t sum = 0;
+
+    boost::timer::cpu_timer st_timer;
+
+    for (int i = 0; i <= N; ++i)
+        sum += i;
+
+    std::cout << "Single thread calculated sum of " << sum << " in\n";
+    std::cout << st_timer.format() << '\n';
+
+    sum = 0;
+    boost::timer::cpu_timer mt_timer;
 
     boost::thread_group threads;
-    std::vector<Adder> adders;
+    std::vector<Adder*> adders;
     adders.reserve(NTHREADS);
     for (int i = 0; i < NTHREADS; ++i)
     {
         int start = i * N_PER_THREAD;
         int stop = i == NTHREADS - 1 ? N + 1 : (i + 1) * N_PER_THREAD;
-        adders.push_back(Adder(start, stop));
-        threads.create_thread(boost::ref(adders[i]));
+        adders.push_back(new Adder(start, stop));
+        threads.add_thread(new boost::thread(boost::ref(*adders[i])));
     }
     threads.join_all();
-    std::uint64_t sum = 0;
     for (int i = 0; i < NTHREADS; ++i)
-        sum = sum + adders[i];
+        sum = sum + *adders[i];
 
-    std::cout << sum << '\n';
-    std::cout << timer.format() << '\n';
+    std::cout << NTHREADS << " threads calculated sum of " << sum << " in\n";
+    std::cout << mt_timer.format() << '\n';
     return 0;
 }
